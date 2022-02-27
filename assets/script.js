8598b17b6dfaa121ba527831a833b1a73f1c7952
Colin Christoph <colinchristoph27@gmail.com>
2022-02-27 15:32:27 -0700

    Added questions and answers to variables and updated HTML

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,41 @@
 // Assignment code here
 var Questions = [
-    'Question 1',
-    'Question 2',
-    'Question 3',
-    'Question 4',
-    'Question 5',
+    'Javascript is an _______ language?',
+    'Which of the following methods can be used to display data in some form using Javascript?',
+    'When an operators value is NULL, the type of returned by the unary operator is:',
+    'What does the Javascript “debugger” statement do?',
+    'Which function is used to serialize an object into a JSON string in Javascript?',
 ];
 var Answers = [
-    ''
+    'Object-Oriented',
+    'Object-Based',
+    'Procedural',
+    'None of the above',
+    'document.write()',
+    'console.log()',
+    'window.alert()',
+    'All of the above',
+    'Boolean',
+    'Object',
+    'Undefined',
+    'Interger',
+    'It acts as a breakpoint in a program',
+    'It will debug all the errors in a program',
+    'It will debug the error in the current statement if any',
+    'All of the above',
+    'stringify()',
+    'parse()',
+    'convert()',
+    'None of the above',
 
 ];
+var rightAnswers = [
+    'Object-Oriented',
+    'All of the above',
+    'Boolean',
+    'It acts as a breakpoint in a program',
+    'stringify()',
+]
 var generateBtn = document.querySelector("#start");
 // Wrap most of the Javascript in run quiz function
 function runQuiz() {
